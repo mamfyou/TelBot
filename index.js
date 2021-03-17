beb42ced618355ddcbeb191d0c20cabb5cfe12b5
pmcheraghi <pmahdicheraghi@gmail.com>
2021-03-17 20:09:04 +0330

    Bugs Fixed

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import {Scenes, Telegraf} from 'telegraf';
+import {Scenes, Telegraf, session} from 'telegraf';
 
+const greeterScene = new Scenes.BaseScene('SCENARIO_TYPE_SCENE_ID');
 
+greeterScene.enter((ctx) => {
+    ctx.session.myData = {};
+    ctx.reply('How are you?');
+});
+
+greeterScene.leave((ctx) => {
+    ctx.reply('Thank you for your time!');
+});
+
+// What to do if user entered a raw message or picked some other option?
+greeterScene.use((ctx) => ctx.replyWithMarkdown('Please *choose* either Fine! or Bad!'));
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const stage = new Scenes.Stage([greeterScene]);
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.command('greeter', (ctx) => {
+    console.log(ctx)
+    ctx.scene.enter('SCENARIO_TYPE_SCENE_ID');
+});
 
 bot.start((ctx) => {
     ctx.reply('خوش آمدید!');
