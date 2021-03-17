9c84c20561a73d7af62d3be27cdf359d61849e41
pmcheraghi <pmahdicheraghi@gmail.com>
2021-03-17 15:43:03 +0330

    last edit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const name1 = "Mahdi"
+import { Telegraf } from 'telegraf';
 
-console.log("Hello" + name1)
+const bot = new Telegraf("1637503600:AAE29ldALGVRAsUjGN3sTnbmS3A624ABMtM");
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears("hi", (ctx) => ctx.reply("Hello friends!"));
+bot.on("message", (ctx) => ctx.reply(':/'));
+	
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
