93c874365000113e956ef894e1cd97fa5e36c942
pmcheraghi <pmahdicheraghi@gmail.com>
2021-03-26 11:43:32 +0430

    KAF

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Scenes, Telegraf, Markup, session } from 'telegraf';
 
 const SCENE_NAME = new Scenes.BaseScene("SCENARIO_TYPE_SCENE_ID");
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const stage = new Scenes.stage([SCENE_NAME], ...);
+const stage = new Scenes.Stage([SCENE_NAME], ...);
 
 bot.use(session());
 bot.use(stage.middleware());
@@ -33,9 +33,29 @@ bot.action("UNIQUE_KEY1", (ctx) => {DESIRED_ACTION...};
 
 */
 
+// KAF
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const bookReedScene = new Scenes.BaseScene("BOOK_READ_SCENE");
 
+const stage = new Scenes.Stage([bookReedScene]);
+
+bot.use(session());
+bot.use(stage.middleware());
+
+bot.start((ctx) => {
+    console.log("started!"),
+    ctx.reply("📚به بات کاف خوش آمدید", Markup.inlineKeyboard([
+        Markup.button.callback("کتاب چی بخونم؟", "BOOK_READ_KEY"),
+        Markup.button.callback("کتاب چی قرض بگیرم؟", "BOOK_BORROW_KEY"),
+        Markup.button.callback("کتاب چی بخرم؟", "BOOK_BUY_KEY")
+    ]).resize());
+});
+
+bot.action("BOOK_READ_KEY", (ctx) => ctx.scene.enter("BOOK_READ_SCENE"));
+
+bot.launch();
 
 
 // Enable graceful stop
